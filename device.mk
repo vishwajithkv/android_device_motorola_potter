869737e379e80074989a28b8322e2057970d0ed9
leoxsys <leoxsys@users.noreply.github.com>
2022-05-02 13:34:18 +0530

    potter: build normal variant of vendor.qti.hardware.perf@2.0

--- device.mk
+++ device.mk
@@ -313,6 +313,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.0 \
     android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
